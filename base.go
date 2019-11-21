afea1832e274ae104c1c6298e64e578803414248
akushman <zeusakm@gmail.com>
2019-11-21 08:03:45 +0100

    #1: Add searching min edges in 2 trees in parallel

---
+++ base.go
@@ -0,0 +1,82 @@
+package burovka
+
+import (
+	"math"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+type Vertex struct {
+	Name     string
+	Order    int32
+	Edges    []*Edge
+	Adjacent map[Vertex]*Edge
+	minEdge  *Edge
+}
+
+type Edge struct {
+	Name   string
+	Weight int64
+	Order  int32
+}
+
+// FindMST gets simple weighted undirected graph as an input
+// returns minimum spanning tree as an output
+// Algorithm is based on Burovka's theorem for faster parallel computing
+func FindMST(swug []*Vertex) []*Vertex {
+	l := len(swug)
+	half := l / 2
+	treeOne := swug[:half]
+	treeTwo := swug[half:]
+
+	// process 1st part
+	wg.Add(1)
+	go func() {
+		for _, v := range treeOne {
+			min := int64(math.MaxInt64)
+			for _, e := range v.Edges {
+				if e.Weight < min {
+					min = e.Weight
+					v.minEdge = e
+				}
+			}
+
+			if len(v.Adjacent) > 0 { // check adjacency and try to find foreign
+				minForeign := int64(math.MaxInt64)
+				// search the min connecting edge between 2 trees
+				for _, vTwo := range treeTwo {
+					for _, vOne := range treeOne {
+						if val, ok := vOne.Adjacent[*vTwo]; ok && minForeign > val.Weight {
+							minForeign = val.Weight
+						}
+					}
+				}
+			}
+		}
+		wg.Done()
+	}()
+
+	// process 2nd part - here we don't need to search the min connecting edge between 2 trees
+	wg.Add(1)
+	go func() {
+		for _, v := range treeTwo {
+			min := int64(math.MaxInt64)
+			for _, e := range v.Edges {
+				if e.Weight < min {
+					min = e.Weight
+					v.minEdge = e
+				}
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+
+	return append(treeOne, treeTwo...)
+}
+
+func (swug *Vertex) findMinTree() {
+
+	wg.Done()
+}
